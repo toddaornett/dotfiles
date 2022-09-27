4e8a697ba847aa2e4fffdb8bf1b9c1be4d16db16
toddaornett <mygithub@acquirus.com>
2022-09-27 10:41:24 +0900

    initial commit - start of neovim config

---
+++ nvim/init.lua
@@ -0,0 +1,2 @@
+require('settings')
+require('packer-plugins')
---
+++ nvim/lua/settings.lua
@@ -0,0 +1,10 @@
+-- global options
+vim.g,mapleader = " "
+
+-- general options
+vim.o.relativenumber = true
+
+-- buffer options
+vim.bo.expandtab = true
+vim.bo.shiftwidth = 4
+vim.bo.softtabstop = 4
