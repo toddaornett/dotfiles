a71170a0a9a350938171f4ab7e33d48836525e76
todd <todd@todds-MacBook-Pro.local>
2023-06-02 08:40:20 +0900

    add after! for eglot config

--- doom/config.el
+++ doom/config.el
@@ -102,12 +102,14 @@
 (cl-defmethod project-root ((project (head eglot-project)))
   (cdr project))
 
-(defun my-project-try-tsconfig-json (dir)
-  (when-let* ((found (locate-dominating-file dir "tsconfig.json")))
-    (cons 'eglot-project found)))
+(after! eglot
+  (defun my-project-try-tsconfig-json (dir)
+    (when-let* ((found (locate-dominating-file dir "tsconfig.json")))
+      (cons 'eglot-project found)))
 
-(add-hook 'project-find-functions
-          'my-project-try-tsconfig-json nil nil)
+  (add-hook 'project-find-functions
+            'my-project-try-tsconfig-json nil nil)
 
-(add-to-list 'eglot-server-programs
-             '((typescript-mode) "typescript-language-server" "--stdio"))
+  (add-to-list 'eglot-server-programs
+               '((typescript-mode) "typescript-language-server" "--stdio"))
+  )
