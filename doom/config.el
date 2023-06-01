4fe038e3b49cf92856399fe15e37312d566f803b
todd <todd@todds-MacBook-Pro.local>
2023-06-01 14:18:52 +0900

    add eglot for better lsp integration

--- doom/config.el
+++ doom/config.el
@@ -97,3 +97,17 @@
         t))))
 
 (add-hook 'focus-out-hook (lambda () (save-some-buffers t)))
+
+;; I'm not sure why this is needed, but it throws an error if I remove it
+(cl-defmethod project-root ((project (head eglot-project)))
+  (cdr project))
+
+(defun my-project-try-tsconfig-json (dir)
+  (when-let* ((found (locate-dominating-file dir "tsconfig.json")))
+    (cons 'eglot-project found)))
+
+(add-hook 'project-find-functions
+          'my-project-try-tsconfig-json nil nil)
+
+(add-to-list 'eglot-server-programs
+             '((typescript-mode) "typescript-language-server" "--stdio"))
