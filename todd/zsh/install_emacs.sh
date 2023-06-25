9bbe01318be44829e50ee416c9e31c8e3e02d499
todd <todd@todds-MacBook-Pro.local>
2023-06-25 10:49:15 +0900

    updated emacs installation and script to install

---
+++ todd/zsh/install_emacs.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+brew install autoconf
+brew install automake
+brew install coreutils
+brew install dbus
+brew install expat
+brew install gcc
+brew install gmp
+brew install gnu-sed
+brew install gnutls
+brew install jansson
+brew install libffi
+brew install libgccjit
+brew install libiconv
+brew install librsvg
+brew install libtasn1
+brew install libunistring
+brew install libxml2
+brew install little-cms2
+brew install mailutils
+brew install make
+brew install ncurses
+brew install nettle
+brew install pkg-config
+brew install sqlite
+brew install texinfo
+brew install tree-sitter
+brew install zlib
+cd ~/Projects
+git clone https://github.com/emacs-mirror/emacs.git
+cd emacs
+autoreconf -isvf
+./configure --with-native-compilation --prefix=/opt/emacs
+make -j10
+make check
+
